6d3fce1c4bb3072b59c2d172d8e9d6e38c8a6a22
lucasrpinto <lucasrop22@gmail.com>
2025-03-26 10:24:22 -0300

    Chore: disabled SQLAlchemy track modifications

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models.snack import Snack
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@127.0.0.1:3306/dayly_diet'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
