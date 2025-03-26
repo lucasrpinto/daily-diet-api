e1558c7056ae5e031865690c076b9b502ffa8907
lucasrpinto <lucasrop22@gmail.com>
2025-03-26 10:16:14 -0300

    Feat: Create first route of create diet and config the database

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
+
 from database import db
+from models.snack import Snack
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -7,10 +9,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@127.0.0.1:3
 
 db.init_app(app)
 
-@app.route('/hello-world', methods=['GET'])
-def hello_world():
-    return "Hello World!"
+# Rotas
+@app.route('/diet', methods=['POST'])
+def create_diet():
+    data = request.json
+    name = data.get("name")
+    description = data.get("description")
 
+    if name and description:
+        snack = Snack(name=name, description=description, diet=True)
+        db.session.add(snack)
+        db.session.commit()
+        return jsonify({'message': 'Dieta cadastrada com sucesso!'}), 201
+    
+    return jsonify({'message': 'Credenciais inválidas'}), 400
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Snack': Snack}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/snack.py
+++ models/snack.py
@@ -1,9 +1,9 @@
-from pymysql import Date
+from datetime import date
 from database import db
 
 class Snack(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(30), nullable=False)
-    description = db.Column(db.text, nullable=False)
-    date = db.Column(db.Date, default=Date.utcnow, nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.Date, default=date.today, nullable=False)
     diet = db.Column(db.Boolean, default=True)
